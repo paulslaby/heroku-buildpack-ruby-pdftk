dde8cfeb8605af79e6125769de016f666083c1ba
Pavel Slabý <paul.slaby@seznam.cz>
2017-08-23 10:14:40 +0200

    Update S3 path to pdftk.tar.gz

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -6,7 +6,7 @@ class Pdftk < BaseCustom
     "pdftk"
   end
   def source_url
-    "http://s3.amazonaws.com/source_url_here"
+    "https://s3.eu-central-1.amazonaws.com/elfie-graphics-production/pdftk.tar.gz"
   end
   def used?
     File.exist?("#{build_path}/bin/pdftk") && File.exist?("#{build_path}/bin/lib/libgcj.so.12")
